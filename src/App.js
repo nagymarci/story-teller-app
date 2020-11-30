2216601afdeb41f0187369d7fdf4f0192b3dbbb7
Marton Nagy <marton.nagy@logmein.com>
2020-11-30 23:33:42 +0100

    feat: make story shareable

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
+import React, { useState } from 'react';
 import { StoryTeller } from './StoryTeller';
 import { Container, Row, Button } from 'react-bootstrap';
 
 function App() {
 
-  const handleClick = () => {
-    window.location.reload()
-  }
-
   return (
     <Container className="App">
-      <Row className="mt-1">
-        <Button onClick={handleClick}>Új mese</Button>
-      </Row>
       <Row className="mt-5">
       <StoryTeller />
       </Row>
--- src/StoryTeller.js
+++ src/StoryTeller.js
@@ -1,40 +1,62 @@
 import React, { useState, useEffect } from 'react';
-import { Table } from 'react-bootstrap';
+import { Table, Button, Container, Row } from 'react-bootstrap';
 
 export const StoryTeller = () => {
   const [gameState, setGameState] = useState(null)
   const [isLoading, setLoading] = useState(true)
+  const [gameId, setGameId] = useState(window.location.hash.substring(2))
+
+  console.log(gameId)
   
   useEffect(() => {
-    if (isLoading && gameState == null) {
-      fetch(window.apiUrl, {
-        method: "post"
-      }).then((res) => Promise.all([res.status, res.json()]))
+    if (isLoading && gameId !== "") {
+      fetch(window.apiUrl + "/" + gameId).then((res) => Promise.all([res.status, res.json()]))
       .then(([status, result]) => {
         console.log(status, result)
         setGameState(result)
         setLoading(false)
       })
     }
-  })
+  }, [isLoading, gameState, gameId])
 
-  if (isLoading) {
-    return "Loading"
+  const handleClick = () => {
+    fetch(window.apiUrl, {
+      method: "post"
+    }).then((res) => Promise.all([res.status, res.json()]))
+    .then(([status, result]) => {
+      console.log(status, result)
+      window.location.hash = "/" + result.id
+      console.log(window.location.hash)
+      setGameId(result.id)
+      setLoading(true)
+    })
+  }
+
+  if (isLoading && gameId !== "") {
+    return "Mese összetevőinek készítése..."
   }
 
   return (
-    <Table bordered>
-      <tbody>
-      <tr>
-      {gameState.emojis.map((emoji) => {
-        return (
-          <td key={emoji.id}>
-            {emoji.symbol}
-            </td>
-        )
-      })}
-      </tr>
-      </tbody>
-    </Table>
+    <Container className="StoryTeller">
+      <Row className="mt-1">
+        <Button onClick={handleClick}>Új mese</Button>
+      </Row>
+      {gameId !== "" && 
+      <Row className="mt-5">
+        <Table bordered>
+        <tbody>
+        <tr>
+        {gameState.emojis.map((emoji) => {
+          return (
+            <td key={emoji.id}>
+              {emoji.symbol}
+              </td>
+          )
+        })}
+        </tr>
+        </tbody>
+        </Table>
+      </Row> }
+    </Container>
   )
 }
