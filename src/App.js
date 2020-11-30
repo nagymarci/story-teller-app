b83d40a45c73988b64ee0808cedd47ada93b5429
Marton Nagy <marton.nagy@logmein.com>
2020-11-30 21:55:11 +0100

    feat: add basic functionality

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { StoryTeller } from './StoryTeller';
+import { Container, Row, Button } from 'react-bootstrap';
+
+function App() {
+
+  const handleClick = () => {
+    window.location.reload()
+  }
+
+  return (
+    <Container className="App">
+      <Row className="mt-1">
+        <Button onClick={handleClick}>New Game</Button>
+      </Row>
+      <Row className="mt-5">
+      <StoryTeller />
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/StoryTeller.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { Table } from 'react-bootstrap';
+
+export const StoryTeller = () => {
+  const [gameState, setGameState] = useState(null)
+  const [isLoading, setLoading] = useState(true)
+  
+  useEffect(() => {
+    if (isLoading && gameState == null) {
+      fetch("http://localhost:4400/story", {
+        method: "post"
+      }).then((res) => Promise.all([res.status, res.json()]))
+      .then(([status, result]) => {
+        console.log(status, result)
+        setGameState(result)
+        setLoading(false)
+      })
+    }
+  })
+
+  if (isLoading) {
+    return "Loading"
+  }
+
+  return (
+    <Table bordered>
+      <tbody>
+      <tr>
+      {gameState.emojis.map((emoji) => {
+        return (
+          <td key={emoji.id}>
+            {emoji.symbol}
+            </td>
+        )
+      })}
+      </tr>
+      </tbody>
+    </Table>
+  )
+}
